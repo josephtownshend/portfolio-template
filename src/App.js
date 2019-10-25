0bfe43713215daac7c48ebcc8dbbd7e3a4a22c17
joseph townshend <joetownshend@mac.com>
2019-10-25 15:06:54 +0100

    setup: header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Slideshow from './components/Slideshow';
+import Header from './components/Header';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Slideshow />
     </div>
   );
