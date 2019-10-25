873ae0e52b0a464710a74136ec7fa7be841a6ef9
joseph townshend <joetownshend@mac.com>
2019-10-25 14:53:36 +0100

    setup: more images added to state

--- src/components/Slideshow.js
+++ src/components/Slideshow.js
@@ -3,7 +3,12 @@ import React from 'react';
 export default class Slideshow extends React.Component {
 
   state = {
-    images: ["http://www.fruttagallery.com/wp-content/uploads/1.LaurenKeeley_InaYear_2015_Frutta.jpg"]
+    images: [
+      "http://www.fruttagallery.com/wp-content/uploads/1.LaurenKeeley_InaYear_2015_Frutta.jpg",
+      "http://www.fruttagallery.com/wp-content/uploads/2.LaurenKeeley_InaYear_2015_Frutta.jpg",
+      "http://www.fruttagallery.com/wp-content/uploads/3.LaurenKeeley_InaYear_2015_Frutta.jpg",
+      "http://www.fruttagallery.com/wp-content/uploads/4.LaurenKeeley_InaYear_2015_Frutta.jpg"
+    ]
   };
 
   render() {
