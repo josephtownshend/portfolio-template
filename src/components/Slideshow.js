afe9cd05e8b5cd7a11dfab505847cd747490a0fe
joseph townshend <joetownshend@mac.com>
2019-10-25 14:51:58 +0100

    feat: image component returning 1 image

--- src/components/Slideshow.js
+++ src/components/Slideshow.js
@@ -2,9 +2,15 @@ import React from 'react';
 
 export default class Slideshow extends React.Component {
 
+  state = {
+    images: ["http://www.fruttagallery.com/wp-content/uploads/1.LaurenKeeley_InaYear_2015_Frutta.jpg"]
+  };
+
   render() {
     return(
-      <div> SlideShow!</div>
-    )
+      <div>
+        <img style={{width:1000}} src={this.state.images[0]} alt="Lauren Keeley"/>
+      </div>
+    );
   }
 }
