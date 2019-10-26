1db80846718609e349c5e4bf9171c04f01d7b5d5
joseph townshend <joetownshend@mac.com>
2019-10-26 13:56:53 +0100

    feat: changed order of nav buttons

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,9 +8,9 @@ export default class Header extends React.Component {
       <React.Fragment>
         <div className="NavBar">
           <button onClick={this.props.handleChangeTitle} className="Button_title">Artist Website</button>
+          <button onClick={this.props.handleChangeContact} className="Button_subMenu">Contact</button>
           <button onClick={this.props.handleChangeAbout} className="Button_subMenu">About</button>
           <button onClick={this.props.handleChangeWriting} className="Button_subMenu">Writing</button>
-          <button onClick={this.props.handleChangeContact} className="Button_subMenu">Contact</button>
         </div>
       </React.Fragment>
     );
