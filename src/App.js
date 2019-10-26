e2f032d139e9bedffea952bff02cc28454e729fe
joseph townshend <joetownshend@mac.com>
2019-10-26 10:25:51 +0100

    feat: header rendering alongside other components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Slideshow from './components/Slideshow';
 import Header from './components/Header';
 import Writing from './components/Writing';
+import About from './components/About';
 import './App.css';
 
 export default class App extends React.Component {
@@ -14,21 +15,24 @@ export default class App extends React.Component {
     if (this.state.whichComponentToShow === "Slideshow") {
       return (
         <div className="App">
+          <Header />
           <Slideshow />
         </div>
       )
     } else if (this.state.whichComponentToShow === "Writing") {
       return (
         <div className="App">
+          <Header />
           <Writing />
         </div>
       )
     } else if (this.state.whichComponentToShow === "About") {
       return (
         <div className="App">
+          <Header />
           <About />
         </div>
       )
     }
-  }  
+  }
 }
