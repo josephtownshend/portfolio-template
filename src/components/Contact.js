0ee986832158e6a4dcb6ee367f2441221f7f6561
joseph townshend <joetownshend@mac.com>
2019-10-28 19:33:56 +0000

    setup: added enzyme to project

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,7 +5,22 @@ export default class Contact extends React.Component {
 
   render() {
     return(
-      <div>email@gallery.com</div>
+      <div>
+        <p className="BlockText">
+          For all sales enquires please get in touch with <a href = "mailto: info@gallery.com">Gallery Gallery</a>.
+        </p>
+        <p className="BlockText">
+          For all other enquires please get in touch <a href = "mailto: info@personal.com">directly</a>.
+        </p>
+        <br />
+        <p className="BlockText">
+          My Artist Studio <br />
+          123 Studio <br/>
+          The Studio Building<br/>
+          London<br/>
+          SE15 123
+        </p>
+      </div>
     )
   }
 }
